efe481880c1d9cdada336514bb436dd8a67996c2
VincentVaughn <33636265+VincentVaughn@users.noreply.github.com>
2018-09-29 16:39:05 -0700

    Added controller support
    "Wing Surface, Controller" works with XBOX controllers.

---
+++ Wing Surface, Controller/Assets/PitchUserControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitchUserControl : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        HingeJoint connection = GetComponent<HingeJoint>();
+
+        JointSpring hingeSpring = connection.spring;
+
+        //float verticalInput = Input.GetAxis("Vertical");
+
+        hingeSpring.targetPosition = 30 * Input.GetAxis("Vertical") * (-1);
+
+        connection.spring = hingeSpring;
+    }
+}
---
+++ Wing Surface, Controller/Assets/UserControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserControl : MonoBehaviour {
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        HingeJoint connection = GetComponent<HingeJoint>();
+
+        JointSpring hingeSpring = connection.spring;
+
+        //float verticalInput = Input.GetAxis("Vertical");
+
+        hingeSpring.targetPosition = 30 * Input.GetAxis("Horizontal");
+
+        connection.spring = hingeSpring;
+    }
+}
