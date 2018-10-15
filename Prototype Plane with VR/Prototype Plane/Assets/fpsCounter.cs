ce0a370874146aa50bd685cef890b7835996ec57
Chase <33075376+thechasetrio@users.noreply.github.com>
2018-10-15 15:02:21 -0700

    Added the VR Demo
    Added VR to the Prototype Plane demo

---
+++ Prototype Plane with VR/Prototype Plane/Assets/fpsCounter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpsCounter : MonoBehaviour {
+
+    float lastFrameTime = 0.0f;
+    float currentFrameTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        Application.targetFrameRate = 90;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lastFrameTime = currentFrameTime;
+        currentFrameTime = Time.deltaTime;
+	}
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 100, 100), (2 / (lastFrameTime + currentFrameTime)).ToString());
+    }
+}
